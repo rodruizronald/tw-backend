2ed16ddaed9cc19ce86c6188f8a4b2999c0e7770
Ronald Rodriguez <rodruizronald@gmail.com>
2025-11-07 12:13:52 -0600

    case sensitve search (#14)
    * index lower case tech
    
    * fix search query
    
    * case sensitive tech search
    
    * adjust db populators
    
    * add new technologies
    
    * fix db populators
    
    * fix linter

--- cmd/db_company_populator/main.go
+++ cmd/db_company_populator/main.go
@@ -38,6 +38,7 @@ func main() {
 func run(ctx context.Context) error {
 	// Initialize logger
 	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
 	log.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
